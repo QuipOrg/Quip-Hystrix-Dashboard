74dbc1a01042deca0b5fc00639965fab7a6ea48e
Matheu DeWitt <msdewitt@bsu.edu>
2018-03-24 15:38:36 -0400

    Initial commit.

---
+++ src/main/java/com/evilscheme/hystirxdashboard/HystirxDashboardApplication.java
@@ -0,0 +1,12 @@
+package com.evilscheme.hystirxdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HystirxDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystirxDashboardApplication.class, args);
+	}
+}
